ac09c37384e5ec2ac9d662179d405b7c6a41c5f8
Roshanell Francisco <roshanellf@gmail.com>
2023-01-23 18:35:07 -0600

    working on toggling fuction for light and dark mode

--- recipie/html/js/main.js
+++ recipie/html/js/main.js
@@ -1,1 +1,23 @@
+// change theme from dark to light mode - donw
+// create  button - done
+// add event listener to button -done
+// change background if clicked
+// change html theme with inner text ?
+let themeChangerButton = document.querySelector(".theme-btn");
+let themeChangerAttribute = document.documentElement.attributes.name;
+console.log(themeChangerAttribute);
+themeChangerButton.addEventListener("click", toLightMode);
 
+function toLightMode() {
+	alert("Clicked!");
+	// themeChangerButton.innerText = "Changed to Light Mode";
+
+	// if (themeChangerAttribute.innerText == "dark") {
+	// 	themeChangerAttribute.innerText == "light";
+	// } else {
+	// 	themeChangerAttribute.innerText == "dark";
+	// }
+
+	// console.log(themeChangerElement)
+	//document.querySelector("data-theme").innerText = "light"
+}
