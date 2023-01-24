72a59713ef6b733074737ba63fa92e232e3d5e5d
Roshanell Francisco <roshanellf@gmail.com>
2023-01-24 13:02:27 -0600

    Added lightmode text to button on click

--- recipie/html/js/main.js
+++ recipie/html/js/main.js
@@ -1,15 +1,18 @@
-//objective: change theme from dark to light mode -
+// change theme from dark to light mode - donw
 // create  button - done
 // add event listener to button -done
 // change background if clicked
-// change html theme with inner text ?
+// change html theme with inner text 
+
+
+
 
 let themeChangerButton = document.querySelector(".theme-btn");
 themeChangerButton.addEventListener("click", toLightMode);
 
 function toLightMode() {
 	alert("Clicked!");
-
+	themeChangerButton.innerText = "Changed to Light Mode";
 	const card = document.querySelector("#card");
 	card.dataset.theme = "light";
 }
