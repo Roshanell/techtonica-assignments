2fcd664ecea8447f8ed3253725761cea8d770d98
Roshanell Francisco <roshanellf@gmail.com>
2023-01-24 12:51:39 -0600

    light mode, event listener, and alert  working

--- recipie/html/js/main.js
+++ recipie/html/js/main.js
@@ -1,23 +1,15 @@
-// change theme from dark to light mode - donw
+//objective: change theme from dark to light mode -
 // create  button - done
 // add event listener to button -done
 // change background if clicked
 // change html theme with inner text ?
+
 let themeChangerButton = document.querySelector(".theme-btn");
-let themeChangerAttribute = document.documentElement.attributes.name;
-console.log(themeChangerAttribute);
 themeChangerButton.addEventListener("click", toLightMode);
 
 function toLightMode() {
 	alert("Clicked!");
-	// themeChangerButton.innerText = "Changed to Light Mode";
 
-	// if (themeChangerAttribute.innerText == "dark") {
-	// 	themeChangerAttribute.innerText == "light";
-	// } else {
-	// 	themeChangerAttribute.innerText == "dark";
-	// }
-
-	// console.log(themeChangerElement)
-	//document.querySelector("data-theme").innerText = "light"
+	const card = document.querySelector("#card");
+	card.dataset.theme = "light";
 }
