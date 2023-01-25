64332ed1593f09ce87c3a173ca9c6eb9d5f95abb
Roshanell Francisco <roshanellf@gmail.com>
2023-01-24 21:35:27 -0600

    adding mouseover and out effect on popular badge

--- recipie/html/js/main.js
+++ recipie/html/js/main.js
@@ -2,10 +2,7 @@
 // create  button - done
 // add event listener to button -done
 // change background if clicked
-// change html theme with inner text 
-
-
-
+// change html theme with inner text
 
 let themeChangerButton = document.querySelector(".theme-btn");
 themeChangerButton.addEventListener("click", toLightMode);
@@ -14,11 +11,16 @@ const card = document.querySelector("#card");
 function toLightMode() {
 	alert("Clicked!");
 	themeChangerButton.innerText = "Changed to Light Mode";
-	 card.dataset.theme = "light"
-// create a toggle button between light and dark mode
-
+	card.dataset.theme = "light";
+	// create a toggle button between light and dark mode
 }
 
-// change height to 40 when hoverin over 
+// change height to 40 when hovering over
 
-
+function bigBadge(x) {
+	x.style.height = "40px";
+	x.style.width = "90px";
+}
+function normalBadge(x) {
+	x.style.height = "20px";
+}
