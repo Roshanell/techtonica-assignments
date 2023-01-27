8c151638f5a9d49f08c04a6586d00cc6cb6b111b
Roshanell Francisco <roshanellf@gmail.com>
2023-01-26 20:07:53 -0600

    added check boxes

--- recipie/html/js/main.js
+++ recipie/html/js/main.js
@@ -24,3 +24,29 @@ function bigBadge(x) {
 function normalBadge(x) {
 	x.style.height = "20px";
 }
+
+// u li
+let body = document.getElementsByTagName("body");
+let instructions = [
+	"Preheat oven to 350 degrees F (175 degrees C) and Grease an 8 inch square pan.",
+
+	"In a large bowl, blend melted butter, sugar and vanilla.",
+	"Beat in eggs one at a time.",
+	"Combine the flour, cocoa, baking powder and salt. Graduallyblend into the egg mixture.",
+	"Spread the batter into the prepared pan",
+	"Bake in preheated oven for 40 to 45 minutes, or until brownies begin to pull away from the sides of the pan",
+	"Let brownies cool, then cut into squares. Enjoy!",
+];
+let ul = document.createElement("ul");
+ul.className = "directions";
+
+for (let i = 0; i < instructions.length; i++) {
+	let newListItem = document.createElement("li");
+	let newCheckmark = document.createElement("input");
+	newCheckmark.setAttribute("type", "checkbox");
+	newListItem.innerHTML = instructions[i];
+	newListItem.appendChild(newCheckmark);
+	ul.appendChild(newListItem);
+}
+
+body[0].appendChild(ul);
