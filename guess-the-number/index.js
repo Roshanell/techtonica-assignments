fa83aa5b46c6639a712c91609d0802e780b727a2
Roshanell Francisco <roshanellf@gmail.com>
2023-02-13 16:43:43 -0600

    adding functionality for sumbit buttom

--- guess-the-number/index.js
+++ guess-the-number/index.js
@@ -44,3 +44,85 @@ gameModeArea.innerHTML = mode;
 
 // get a new random number
 let userPreviousGuesses = [];
+
+function submitAnswer() {
+	// checks to make sure event listener is working
+	alert("Checking Your Guess");
+
+	// var for userinput
+	let userInput = document.getElementById("userInput").value;
+	// change input val to  a num
+	let userInputNumber = parseInt(userInput);
+
+	newValue = userInputNumber;
+
+	// winning and loosing statement
+	//previous guesses
+	console.log("userPreviousGuesses", userPreviousGuesses);
+	const previousGuessesArea = document.querySelector("#previousGuessesArea");
+
+	const winningStatement = "Yay, you guessed correctly";
+	const losingStatement = "Sorry, try again";
+	// defining message Area
+	const messageArea = document.querySelector("#messageArea");
+	// console.log(messageArea);
+	let guessCount = humanScore + computerScore;
+	//compare random number to input val
+	if (randomNumber === userInputNumber) {
+		messageArea.innerHTML = winningStatement;
+		humanScore++;
+		// update the scores on the page
+		humanScoreArea.innerHTML = humanScore;
+		computerScoreArea.innerHTML = computerScore;
+		guessTotalArea.innerHTML = guessCount;
+		guessCount++;
+		userPreviousGuesses.push(userInputNumber);
+		console.log("userPreviousGuesses", userPreviousGuesses);
+		//     for(let i = 0; i < userPreviousGuessess.length; i++){
+		//       console.log(previousGuesses)
+		//previousGuessesArea.innerHTML = i ;
+
+		//     }
+
+		// console.log(userPreviousGuesses);
+		//generate a new num
+		randomNumber = getNewRandomNumber();
+		// console.log(randomNumber, "after");
+	} else if (randomNumber != userInputNumber) {
+		messageArea.innerHTML = losingStatement;
+		computerScore++;
+		// update the scores on the page
+		guessTotalArea.innerHTML = guessCount;
+		guessCount++;
+		humanScoreArea.innerHTML = humanScore;
+		computerScoreArea.innerHTML = computerScore;
+		userPreviousGuesses.push(userInputNumber);
+		console.log("userPreviousGuesses", userPreviousGuesses);
+		// console.log(userPreviousGuesses);
+		previousGuessesArea.innerHTML = userPreviousGuesses;
+	}
+}
+
+function easyLevel() {
+	// between 0 & 4
+	mode = "easy";
+	randomNumber = getNewRandomNumber();
+	console.log(randomNumber);
+}
+
+function mediumLevel() {
+	mode = "medium";
+	gameModeArea.innerHTML = mode;
+
+	randomNumber = getNewRandomNumber();
+	console.log(randomNumber);
+}
+
+function hardLevel() {
+	mode = "hard";
+	gameModeArea.innerHTML = mode;
+
+	randomNumber = getNewRandomNumber();
+
+	console.log(randomNumber);
+}
