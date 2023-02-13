6897333faacd684da8d5385a83847e163f83a268
Roshanell Francisco <roshanellf@gmail.com>
2023-02-13 16:41:23 -0600

    adding score functinality

--- guess-the-number/index.js
+++ guess-the-number/index.js
@@ -26,3 +26,11 @@ submitButton.addEventListener("click", submitAnswer);
 easyButton.addEventListener("click", easyLevel);
 mediumButton.addEventListener("click", mediumLevel);
 hardButton.addEventListener("click", hardLevel);
+
+//score functionality
+let humanScore = 0;
+let computerScore = 0;
+const humanScoreArea = document.getElementById("humanScoreArea");
+const computerScoreArea = document.getElementById("computerScoreArea");
+humanScoreArea.innerHTML = humanScore;
+computerScoreArea.innerHTML = computerScore;
