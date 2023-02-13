eb0ce147df72b48af492fb2d9f5d44619752aff8
Roshanell Francisco <roshanellf@gmail.com>
2023-02-13 16:35:56 -0600

    adding finction get determine the level of of difficulty

--- guess-the-number/index.js
+++ guess-the-number/index.js
@@ -3,3 +3,16 @@ let newValue;
 // default mode
 let mode = "easy";
 let randomNumber = Math.floor(Math.random() * 10);
+
+console.log(randomNumber, "new num");
+// generate random number for mode
+function getNewRandomNumber() {
+	if (mode === "easy") {
+		console.log(mode);
+		return Math.floor(Math.random() * 5);
+	} else if (mode === "medium") {
+		return Math.floor(Math.random() * 10);
+	} else {
+		return Math.floor(Math.random() * 15);
+	}
+}
