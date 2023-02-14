3262fac155f6a542f0351750944a213bfda50c7d
Roshanell Francisco <roshanellf@gmail.com>
2023-02-14 16:06:11 -0600

    generating random image from memesDatabase  in console when clicked

--- meme-generator/src/component/Meme.js
+++ meme-generator/src/component/Meme.js
@@ -1,7 +1,23 @@
+import memesData from "./memesData";
+
 function Meme() {
+	function getRandomImage() {
+		console.log("clicked");
+		//generate a random number (x)
+		const memesArr = memesData.data.memes;
+		let randomNum = [Math.floor(Math.random() * memesArr.length) + 1];
+		console.log(randomNum);
+		//after clicked use that number to grab the x image out of the arr
+        //obj with url
+        let url = memesArr[randomNum].url
+        console.log(url)
+		// create an image section
+
+		// disply it in the image section
+	}
 	return (
 		<div>
-			<form>
+			<main>
 				<div className="inputArea">
 					<input className="wordInputSection" placeholder="Enter Text Here " />
 					<input
@@ -10,12 +26,12 @@ function Meme() {
 					/>
 				</div>
 				<div className="buttonContainer">
-					<button>
+					<button onClick={getRandomImage}>
 						<img src="https://i.imgur.com/x5xcJNa.png" className="imageIcon" />
-							Generate a new meme image{" "}
+						Generate a new meme image{" "}
 					</button>
 				</div>
-			</form>
+			</main>
 		</div>
 	);
 }
