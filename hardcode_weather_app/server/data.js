92653fa8e1faefe478e63bfc32f8f250488d6c83
Roshanell Francisco <96213223+Roshanell@users.noreply.github.com>
2023-02-28 22:01:33 -0600

    updated hard coded data to houston data

--- hardcode_weather_app/server/data.js
+++ hardcode_weather_app/server/data.js
@@ -1,41 +1,46 @@
 const coord = {
-    lon: -122.3321,
-    lat: 47.6062,
-    weather: [{
-        id: 803,
-        main: "Clouds",
-        description: "broken clouds",
-        icon: "04d",
-        base: "stations",
-        main: {
-            temp: 79.38,
-            feels_like: 75.74,
-            temp_min: 77.4,
-            pressure: 1000,
-            humidity: 78
-            },
-        visibility: 10000,
-        wind: {
-                speed: 5.66,
-                deg: 200
-            },
-        clouds: {
-            all: 75
-        },
-        dt: 1677006621,
-        sys: {
-            type: 2,
-            id: 2041694,
-            country: "US",
-            sunrise: 1676991905,
-            sunset: 1677030070
-            },
-        timezone: -28800,
-        id: 5809844,
-        name: "Seattle",
-        cod: 200
-        }
-    ]
-}
+	coord: {
+		lon: -95.3633,
+		lat: 29.7633,
+	},
+	weather: [
+		{
+			id: 701,
+			main: "Mist",
+			description: "mist",
+			icon: "50n",
+		},
+	],
+	base: "stations",
+	main: {
+		temp: 295.49,
+		feels_like: 296.05,
+		temp_min: 294.77,
+		temp_max: 296.15,
+		pressure: 1009,
+		humidity: 87,
+	},
+	visibility: 8047,
+	wind: {
+		speed: 6.69,
+		deg: 140,
+		gust: 9.26,
+	},
+	clouds: {
+		all: 100,
+	},
+	dt: 1677643132,
+	sys: {
+		type: 2,
+		id: 2001415,
+		country: "US",
+		sunrise: 1677588523,
+		sunset: 1677629980,
+	},
+	timezone: -21600,
+	id: 4699066,
+	name: "Houston",
+	cod: 200,
+};
 
 module.exports = coord;
