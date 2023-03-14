7925343eb7de0a26b5320a574043783938e55d6e
Roshanell Francisco <96213223+Roshanell@users.noreply.github.com>
2023-03-14 16:37:03 -0500

    can toggle favorite

--- eventonica/client/src/components/event.js
+++ eventonica/client/src/components/event.js
@@ -1,18 +1,39 @@
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 import Moment from "react-moment";
+import { useState } from "react";
 
-const EventCard = (props) =>{
-
-    return(
-    <Card style={{ width: '18rem' }}>
-    <Card.Body>
-      <Card.Title>{props.title}</Card.Title>
-      <Card.Subtitle className="mb-2 text-muted">Date: {!props.time ? "TBD" : <Moment format={"DD/MM/YYYY"}>{props.time}</Moment>}</Card.Subtitle>
-      <Card.Text>
-        {props.location}
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  )}
+const EventCard = (props) => {
+	const [isFavorite, setIsFavorite] = useState(true);
+	const handleFavoriteClick = () => {
+		console.log("click");
+		setIsFavorite((prevVal) => !prevVal);
+	};
+	const favorite = <i className="fa-solid fa-heart"> </i>;
+	const isNotFavorite = <i className="fa-regular fa-heart"></i>;
+	return (
+		<Card style={{ width: "18rem" }}>
+			<Card.Body>
+				<Card.Title>
+					{props.title}
+					<div style={{ backgroundColor: "green" }}>
+						<span onClick={handleFavoriteClick}>
+							{" "}
+							{isFavorite ? favorite : isNotFavorite}
+						</span>
+					</div>
+				</Card.Title>
+				<Card.Subtitle className="mb-2 text-muted">
+					Date:{" "}
+					{!props.time ? (
+						"TBD"
+					) : (
+						<Moment format={"DD/MM/YYYY"}>{props.time}</Moment>
+					)}
+				</Card.Subtitle>
+				<Card.Text>{props.location}</Card.Text>
+			</Card.Body>
+		</Card>
+	);
+};
 
 export default EventCard;
